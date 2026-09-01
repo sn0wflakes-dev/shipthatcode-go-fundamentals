b8bc323cbadc401e623eca7ee389dabe3a5acd23
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-01 16:01:34 +0700

    done: Variable and Types 02

--- main.go
+++ main.go
@@ -4,5 +4,14 @@ import "fmt"
 
 func main() {
     // Print the greeting below.
-		fmt.Println("Hello, Go!")
+		
+		// exercise 01 - Hello, World!
+		// fmt.Println("Hello, Go!")
+
+		// exercise 02 - Variable and Types
+		var num1, num2 int
+		fmt.Scan(&num1, &num2)
+
+		result := num1 + num2
+		fmt.Println(result)
 }
